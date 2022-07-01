43f409d31a401b8f974492d3e733a8763f59a35e
kansan-kansan <2011140063k@ed.fuk.kindai.ac.kp>
2022-07-01 15:55:21 +0900

    #heroku用

---
+++ app.py
@@ -0,0 +1,15 @@
+#https://flask.palletsprojects.com/en/2.0.x/quickstart/
+
+from click import style
+from flask import Flask
+from flask import render_template
+app = Flask(__name__, static_folder='./templates/images')#アプリのインスタンス化
+
+
+@app.route("/") #URLを作っているイメージ
+def hello():  #関数名はなんでも良い
+    return render_template('hello.html')
+    #city左側はhello.html内の変数
+
+if __name__=="__main__":
+    app.run(debug=True)
